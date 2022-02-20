dd9e1bd31895cf6e24ea0e3f1eb2862622596a88
Shashank <21je0871@iitism.ac.in>
2022-02-20 06:37:33 +0530

    LazyVstack used

--- Recipe List App/Views/ContentView.swift
+++ Recipe List App/Views/ContentView.swift
@@ -13,37 +13,40 @@ struct ContentView: View {
       @EnvironmentObject var model:RecipeModel
       
       var body: some View {
-          
-          NavigationView {
-              List(model.recipes) { r in
-                  
-                  NavigationLink(
-                      destination: RecipeDetailView(recipe:r),
-                      label: {
-                          
-                          // MARK: Row item
-                          HStack(spacing: 20.0) {
-                              Image(r.image)
-                                  .resizable()
-                                  .scaledToFill()
-                                  .frame(width: 50, height: 50, alignment: .center)
-                                  .clipped()
-                                  .cornerRadius(5)
-                              Text(r.name)
-                          }
-                          
-                      })
-                  
-                  
-                  
-              }
-              .navigationBarTitle("All Recipes")
-          }
+            
+            NavigationView {
+                  VStack(alignment:.leading)
+                  {
+                        Text("All Recipes").fontWeight(.bold).font(.largeTitle)
+                        ScrollView {
+                              LazyVStack(alignment:.leading) {
+                                    ForEach(model.recipes)
+                                    { r in
+                                          NavigationLink(
+                                                destination: RecipeDetailView(recipe:r),
+                                                label: {
+                                                      
+                                                      // MARK: Row item
+                                                      HStack(spacing: 20.0) {
+                                                            Image(r.image)
+                                                                  .resizable()
+                                                                  .scaledToFill()
+                                                                  .frame(width: 50, height: 50, alignment: .center)
+                                                                  .clipped()
+                                                                  .cornerRadius(5)
+                                                            Text(r.name)
+                                                      }
+                                                })
+                                    }
+                              }
+                        }
+                  }.navigationBarHidden(true).padding()
+            }
       }
-  }
+}
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+      static var previews: some View {
+            ContentView().environmentObject(RecipeModel())
+      }
 }
